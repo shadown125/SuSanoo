4923195b60dd7d98d7b19727d8d2c384245fad72
YoLoN <shadyolown13@onet.pl>
2022-10-20 14:37:34 +0200

    :art: Add more dummy dates in seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,9 +11,37 @@ const main = async () => {
             name: "Dawid Ol",
             email: "do@example.com",
             password: hashedPassword,
+            image: "/dummy-image.jpg",
+            created_at: new Date().toLocaleDateString(),
         },
         update: {},
         where: { email: "do@example.com" },
+    });
+
+    await prisma.user.upsert({
+        create: {
+            id: "o1j23h12ijh4i2yu1g",
+            name: "Chuck Norris",
+            email: "chuckNoriss@gigachad.com",
+            password: hashedPassword,
+            image: "/dummy-image.jpg",
+            created_at: new Date().toLocaleDateString(),
+        },
+        update: {},
+        where: { email: "chuckNoriss@gigachad.com" },
+    });
+
+    await prisma.user.upsert({
+        create: {
+            id: "njou12iph3291h30123",
+            name: "Itachi Uchiha",
+            email: "itachiU@sharingan.com",
+            password: hashedPassword,
+            image: "/dummy-image.jpg",
+            created_at: new Date().toLocaleDateString(),
+        },
+        update: {},
+        where: { email: "itachiU@sharingan.com" },
     });
 };
 
