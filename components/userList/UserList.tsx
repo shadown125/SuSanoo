949402a9c4cf760c2b7deded4791c08a95227b54
YoLoN <shadyolown13@onet.pl>
2022-10-21 15:07:45 +0200

    :art: Add animation while Filtering in user list section

--- components/userList/UserList.tsx
+++ components/userList/UserList.tsx
@@ -3,6 +3,7 @@ import { trpc } from "../../src/utils/trpc";
 import Image from "next/future/image";
 import Filter from "./Filter";
 import { useTranslation } from "next-i18next";
+import { motion, AnimatePresence } from "framer-motion";
 
 export enum FilteredStates {
     All,
@@ -72,30 +73,32 @@ const UserList: FC = () => {
                                             <th>{t("common:action")}</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        {users!.map((user, index) => (
-                                            <tr key={index}>
-                                                <th>
-                                                    <div className="user-profile">
-                                                        <div className="image-wrapper" style={setProfileStatusColor(user.status)}>
-                                                            <Image src={user.image as string} sizes="100vw" width={100} height={100} alt="Profile image" />
+                                    <motion.tbody layout>
+                                        <AnimatePresence>
+                                            {users!.map((user, index) => (
+                                                <motion.tr layout animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} key={index}>
+                                                    <th>
+                                                        <div className="user-profile">
+                                                            <div className="image-wrapper" style={setProfileStatusColor(user.status)}>
+                                                                <Image src={user.image as string} sizes="100vw" width={100} height={100} alt="Profile image" />
+                                                            </div>
+                                                            <div className="info">
+                                                                <div className="name">{user.name}</div>
+                                                                <div className="email">{user.email}</div>
+                                                            </div>
                                                         </div>
-                                                        <div className="info">
-                                                            <div className="name">{user.name}</div>
-                                                            <div className="email">{user.email}</div>
-                                                        </div>
-                                                    </div>
-                                                </th>
-                                                <td>{user.role}</td>
-                                                <td>{user.created_at}</td>
-                                                <td>
-                                                    <button className="button is-primary">
-                                                        <span>{t("common:edit")}</span>
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
+                                                    </th>
+                                                    <td>{user.role}</td>
+                                                    <td>{user.created_at}</td>
+                                                    <td>
+                                                        <button className="button is-primary">
+                                                            <span>{t("common:edit")}</span>
+                                                        </button>
+                                                    </td>
+                                                </motion.tr>
+                                            ))}
+                                        </AnimatePresence>
+                                    </motion.tbody>
                                 </table>
                             </div>
                         ) : (
