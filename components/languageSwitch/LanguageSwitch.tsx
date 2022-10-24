834d844ebec36a694b9e7a2ebbd1a2d7c6fec830
YoLoN <shadyolown13@onet.pl>
2022-10-24 15:54:16 +0200

    :sparkles: Add languages switch

---
+++ components/languageSwitch/LanguageSwitch.tsx
@@ -0,0 +1,66 @@
+import { FC, useState } from "react";
+import Image from "next/future/image";
+import { motion, Variants } from "framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+import { languages } from "../../content/languages/languages";
+
+const LanguageSwitch: FC = () => {
+    const { t } = useTranslation("common");
+    const router = useRouter();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const menuItemVariants: Variants = {
+        open: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                duration: 0.4,
+
+                ease: [0.6, 0.05, -0.01, 0.9],
+            },
+            display: "block",
+        },
+        closed: {
+            y: -50,
+            opacity: 0,
+            transition: {
+                duration: 0.4,
+                ease: [0.6, 0.05, -0.01, 0.9],
+            },
+            transitionEnd: {
+                display: "none",
+            },
+        },
+    };
+
+    const getActiveLanguage = () => languages.filter((item) => item.locale === router.locale)[0];
+
+    const getAllAvailableLanguages = () => languages.filter((item) => item.locale !== router.locale);
+
+    const toggleOpenMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <motion.div className="language-switch" onClick={toggleOpenMenu}>
+            <div className={`language is-icon${isOpen ? " open" : ""}`}>
+                <Image src={`/${getActiveLanguage()!.icon}`} sizes="100vw" width={50} height={50} alt="Language image" />
+                <span>{t(getActiveLanguage()!.name)}</span>
+            </div>
+            <motion.div className="sub-menu" initial="closed" animate={isOpen ? "open" : "closed"} variants={menuItemVariants}>
+                {getAllAvailableLanguages().map((item, index) => (
+                    <Link href={router.pathname} locale={item.locale} key={index}>
+                        <div className="language">
+                            <Image src={`/${item.icon}`} sizes="100vw" width={50} height={50} alt="Language image" />
+                            <span>{t(item.name)}</span>
+                        </div>
+                    </Link>
+                ))}
+            </motion.div>
+        </motion.div>
+    );
+};
+
+export default LanguageSwitch;
---
+++ content/languages/languages.ts
@@ -0,0 +1,12 @@
+export const languages = [
+    {
+        name: "polish",
+        icon: "poland-icon.svg",
+        locale: "pl",
+    },
+    {
+        name: "english",
+        icon: "united-kingdom-icon.svg",
+        locale: "en",
+    },
+];
