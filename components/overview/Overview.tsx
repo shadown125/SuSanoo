e9706ad3e7249efa0f5b1193704bb9455825d790
YoLoN <shadyolown13@onet.pl>
2022-10-27 17:36:49 +0200

    :art: Output dynamically online users count

--- components/overview/Overview.tsx
+++ components/overview/Overview.tsx
@@ -2,17 +2,27 @@ import { FC } from "react";
 import { useInView } from "react-intersection-observer";
 import Charts from "./Charts";
 import { useTranslation } from "next-i18next";
+import { trpc } from "../../src/utils/trpc";
 
 const Overview: FC = () => {
     const { ref, inView } = useInView();
     const { t } = useTranslation("");
 
+    const { data: onlineUsersAmount } = trpc.useQuery(["auth.getOnlineUsersAmount"]);
+    const { data: users } = trpc.useQuery(["auth.getAllUsers"]);
+
     return (
         <div className="overview" ref={ref}>
             <div className="online-users">
-                <h2 className="headline h4">{t("panel:overview.title")}: 2</h2>
+                <h2 className="headline h4">
+                    {t("panel:overview.title")}: {onlineUsersAmount}
+                </h2>
                 <div className="bar">
-                    <span className={`inner-bar${inView ? " is-active" : ""}`}></span>
+                    {users || onlineUsersAmount ? (
+                        <span className={`inner-bar${inView ? " is-active" : ""}`} style={{ width: `${(onlineUsersAmount! / users!.length) * 100}%` }}></span>
+                    ) : (
+                        <span className="inner-bar" style={{ width: "0%" }}></span>
+                    )}
                 </div>
             </div>
             <Charts />
