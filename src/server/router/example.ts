3720c14a4952989b4a20c83ab133a7d0b600161e
YoLoN <shadyolown13@onet.pl>
2022-10-29 18:32:29 +0200

    :fire: Remove not needed files and query's

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -1,21 +1,15 @@
 import { createRouter } from "./context";
 import { z } from "zod";
 
-export const exampleRouter = createRouter()
-  .query("hello", {
+export const exampleRouter = createRouter().query("hello", {
     input: z
-      .object({
-        text: z.string().nullish(),
-      })
-      .nullish(),
+        .object({
+            text: z.string().nullish(),
+        })
+        .nullish(),
     resolve({ input }) {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
+        return {
+            greeting: `Hello ${input?.text ?? "world"}`,
+        };
     },
-  })
-  .query("getAll", {
-    async resolve({ ctx }) {
-      return await ctx.prisma.example.findMany();
-    },
-  });
+});
