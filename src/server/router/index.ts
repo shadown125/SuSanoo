5c6b39fd94ce2750a41de2a1acfd7e19c3d51a3e
Dawid Oleksiuk <shadyolown13@onet.pl>
2022-11-12 21:07:06 +0100

    :hammer: Add inputs router to appRouter

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,8 +5,14 @@ import superjson from "superjson";
 import { exampleRouter } from "./example";
 import { protectedAuthRouter } from "./protected-auth-router";
 import { protectedAuthPageRouter } from "./protected-auth-pages-router";
+import { protectedAuthInputsRouter } from "./protected-auth-inputs-router";
 
-export const appRouter = createRouter().transformer(superjson).merge("example.", exampleRouter).merge("auth.", protectedAuthRouter).merge("auth.pages.", protectedAuthPageRouter);
+export const appRouter = createRouter()
+    .transformer(superjson)
+    .merge("example.", exampleRouter)
+    .merge("auth.", protectedAuthRouter)
+    .merge("auth.pages.", protectedAuthPageRouter)
+    .merge("auth.inputs.", protectedAuthInputsRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
