ba6e8b8c6d67e03fdfb761937db74ea09ba7b951
Dawid Oleksiuk <shadyolown13@onet.pl>
2022-11-12 21:09:17 +0100

    :art: Add dummy select options data to prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,14 +4,16 @@ import { inputs } from "./seed-data/inputs";
 import { pages } from "./seed-data/pages";
 import { users } from "./seed-data/user";
 import { history } from "./seed-data/history";
+import { selectOptions } from "./seed-data/select-options";
 const prisma = new PrismaClient();
 
 const main = async () => {
     await users(prisma);
     await history(prisma);
     await pages(prisma);
-    await inputs(prisma);
     await components(prisma);
+    await inputs(prisma);
+    await selectOptions(prisma);
 };
 
 main();
