2cb1d82963bedc2b23d270733e7377f317527787
Dawid Oleksiuk <shadyolown13@onet.pl>
2022-11-12 14:09:27 +0100

    :art: Add more inputs dummy data

--- prisma/seed-data/inputs.ts
+++ prisma/seed-data/inputs.ts
@@ -19,6 +19,46 @@ export const inputs = async (prisma: PrismaClient) => {
 
     await prisma.input.upsert({
         create: {
+            id: "fsjap231o3123n2123233213214515123",
+            name: "text-2",
+            componentId: "fsjapo3123n2123",
+            type: "text",
+            halfRow: true,
+        },
+        update: {
+            id: "fsjap231o3123n2123233213214515123",
+            name: "text-2",
+            componentId: "fsjapo3123n2123",
+            type: {
+                set: "text",
+            },
+            halfRow: true,
+        },
+        where: { id: "fsjap231o3123n2123233213214515123" },
+    });
+
+    await prisma.input.upsert({
+        create: {
+            id: "fsjap231o3123n2123233213214515124233",
+            name: "text-3",
+            componentId: "fsjapo3123n2123",
+            type: "text",
+            halfRow: true,
+        },
+        update: {
+            id: "fsjap231o3123n2123233213214515124233",
+            name: "text-3",
+            componentId: "fsjapo3123n2123",
+            type: {
+                set: "text",
+            },
+            halfRow: true,
+        },
+        where: { id: "fsjap231o3123n2123233213214515124233" },
+    });
+
+    await prisma.input.upsert({
+        create: {
             id: "6565",
             name: "text",
             type: "text",
