0c684d6b9c8236db01fded7ba964186a29c32d4f
Dawid Oleksiuk <46052374+shadown125@users.noreply.github.com>
2023-09-24 18:39:25 +0200

    Update prisma types export :lipstick:

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,2 @@
-import { PrismaClient } from "@prisma/client";
-import * as trpcNext from "@trpc/server/adapters/next";
-
-export function createContext(opts?: trpcNext.CreateNextContextOptions): Promise<Context>;
-
+export * from "./src/server/router/context";
 export * from ".prisma/client/index.d";
-
-export interface Context {
-    prisma: PrismaClient;
-}
